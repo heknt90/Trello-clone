ba47ede6480900cd7cf70916a3bc064e84643617
Nikita Efremov <heknt90@gmail.com>
2019-12-17 09:00:07 +0300

    Пояснения для вынесенных методов

--- source/js/modules.js
+++ source/js/modules.js
@@ -1,17 +1,20 @@
 'use strict';
 
-function createCaretPlacer(atStart) { 
+function createCaretPlacer(atStart) {  // Функция высшего порядка
+    // В placeCaretAtStart/End в нее передается 1 аргумент bool, означающий, схлопываются ли начало и конец диапохона
+    // Затем при использовании мы передаем туда аргумент el, который содержит node элемент, в который предстоит установить диапозон
     return function(el) {
         el.focus();
-        if (typeof window.getSelection != "undefined"
-                && typeof document.createRange != "undefined") {
-            let range = document.createRange();
-            range.selectNodeContents(el);
-            range.collapse(atStart);
-            let sel = window.getSelection();
-            sel.removeAllRanges();
-            sel.addRange(range);
-        } else if (typeof document.body.createTextRange != "undefined") {
+        // Я так понимаю есть 2 метода, которые работают в разных версиях браузеров
+        if (!(typeof window.getSelection)
+                && !(typeof document.createRange) ) { // Реализация для современных браузеров
+            let range = document.createRange(); // Возвращает новый объект типа Range.
+            range.selectNodeContents(el); // Выбирает Range содержащий контент Node.
+            range.collapse(atStart); // Возвращает Boolean указывающий, находятся ли range's start and на одной и той же позиции.
+            let sel = window.getSelection(); //Метод возвращает объект Selection, представленный в виде диапазона текста, который пользователь выделил на странице.
+            sel.removeAllRanges(); // Removes all ranges from the selection.
+            sel.addRange(range); // A Range object that will be added to the selection.
+        } else if ( !(typeof document.body.createTextRange) ) { // Реализация для IE8-
             let textRange = document.body.createTextRange();
             textRange.moveToElementText(el);
             textRange.collapse(atStart);
